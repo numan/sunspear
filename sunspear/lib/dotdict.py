c332a69feebecf17750ad9646a512cdad36fbd1d
Numan S <numan@7geese.com>
2012-07-21 14:14:27 -0700

    Added comment to give proper credit for dotdictify

--- sunspear/lib/dotdict.py
+++ sunspear/lib/dotdict.py
@@ -1,3 +1,7 @@
+#Originally 'borrowed' from http://stackoverflow.com/questions/3797957/python-easily-access-deeply-nested-dict-get-and-set
+#Some modifications mad to suit the needs of this project
+
+
 class dotdictify(dict):
     def __init__(self, value=None):
         if value is None:
