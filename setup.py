1d332667d92b03e8486f4f3d2de503fcb930cdac
Numan S <numan@7geese.com>
2012-12-07 11:35:23 -0800

    Few enhancements to the setup.py file to allow our custom version of nydus to be used until my PRs are accepted

--- setup.py
+++ setup.py
@@ -2,11 +2,18 @@
 
 from setuptools import setup, find_packages
 
+tests_require=[
+    'nose',
+    'mock',
+    'nydus-riak-fixes',
+]
+
 setup(
     name="sunspear",
     license='Apache License 2.0',
     version="0.1.0-ALPHA",
     description="Activity streams backed by Riak.",
+    zip_safe=False,
     long_description=open('README.rst', 'r').read(),
     author="Numan Sachwani",
     author_email="numan856@gmail.com",
@@ -16,15 +23,14 @@ setup(
     install_requires=[
         'riak==1.5.1',
         'python-dateutil==1.5',
-        'nydus',
+        'nydus-riak-fixes',
     ],
     dependency_links=[
-        'https://github.com/numan/nydus/tarball/7geese-nydus#egg=nydus',
+        'https://github.com/numan/nydus/tarball/7geese-nydus#egg=nydus-riak-fixes',
     ],
-    tests_require=[
-        'nose',
-        'mock'
-    ],
+    tests_require=tests_require,
+    extras_require={"test": tests_require, "nosetests": tests_require},
+    include_package_data=True,
     classifiers=[
         "Intended Audience :: Developers",
         'Intended Audience :: System Administrators',
