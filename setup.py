aa391a93ad886bee9773914cf63ccc08a1b15bbf
Numan S <numan856@gmail.com>
2013-04-01 15:18:45 -0700

    Upgrade nydus

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     test_suite='nose.collector',
     install_requires=[
-        'nydus==0.10.4',
+        'nydus==0.10.5',
         'riak==1.5.1',
         'python-dateutil==1.5',
         'protobuf==2.4.1',
