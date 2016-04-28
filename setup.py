5f0a890f87d1ad6896be7001fb69ca3f5928698c
Numan Sachwani <numan856@gmail.com>
2016-04-28 11:08:19 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ tests_require=[
 setup(
     name="sunspear",
     license='Apache License 2.0',
-    version="0.3.1",
+    version="0.3.2",
     description="Activity streams backed by Riak.",
     zip_safe=False,
     long_description=open('README.rst', 'r').read(),
