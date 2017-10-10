e24563b65e0df49d8bd18a096d212400ca9deb7d
Nicholas Kobald <nick.kobald@gmail.com>
2017-10-10 10:42:02 -0700

    Updates python dateutil install requirements

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     test_suite='nose.collector',
     install_requires=[
         'riak==2.5.4',
-        'python-dateutil==1.5',
+        'python-dateutil>=1.5, != 2.0',
         'protobuf==2.6.1',
         'sqlalchemy==1.1.14',
         'MySQL-python==1.2.5',
